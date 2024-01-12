fc81c23b66e9001a9798672d04da8a07ac555bfe
AndMkh7 <andmkhart77@gmail.com>
2024-01-12 18:19:37 +0400

    Hot fix

--- app/utils/getTeams.tsx
+++ app/utils/getTeams.tsx
@@ -5,7 +5,9 @@ import getStandings from './getStandings';
 export default async function getTeams(): Promise<Team[]> {
     try {
         const standings: Standing[]= await getStandings();
+
         const teams: Team[]=[];
+        
         for(const league of  standings){
             for(const standing of league.league.standings){
                 if(Array.isArray(standing)){
@@ -13,7 +15,7 @@ export default async function getTeams(): Promise<Team[]> {
                         teams.push(team)
                     }
                 } else {
-                    throw new Error('Invalid standings data');
+                    console.error('Invalid standings data for league:', league.league.name);
                 }
             }
         }
