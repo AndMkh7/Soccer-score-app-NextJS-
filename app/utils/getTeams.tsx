fddadbb494e3f353a2ccb90cc570f359d9d56b34
AndMkh7 <andmkhart77@gmail.com>
2024-01-23 20:31:52 +0400

    Added functionality for searching teams

--- app/utils/getTeams.tsx
+++ app/utils/getTeams.tsx
@@ -1,29 +1,28 @@
-import 'server-only';
 import { Standing, Team } from "@/types";
-import getStandings from './getStandings';
+import "server-only";
+import getStandings from "./getStandings";
 
 export default async function getTeams(): Promise<Team[]> {
-    try {
-        const standings: Standing[]= await getStandings();
+  try {
+    const standings: Standing[] = await getStandings();
 
-        const teams: Team[]=[];
-        
-        for(const league of  standings){
-            for(const standing of league.league.standings){
-                if(Array.isArray(standing)){
-                    for(const team of standing){
-                        teams.push(team)
-                    }
-                } else {
-                    console.error('Invalid standings data for league:', league.league.name);
-                }
-            }
+    const teams: Team[] = [];
+
+    for (const league of standings) {
+      for (const standing of league.league.standings) {
+        if (Array.isArray(standing)) {
+          for (const team of standing) {
+            teams.push(team);
+          }
+        } else {
+          console.error("Invalid standings data");
         }
-        return teams;
-
-    } catch (error) {
-        console.error (`Error occurred while fetching teams` , error);
-        throw error;
-    }
+      }
     }
 
+    return teams;
+  } catch (error) {
+    console.error("Error occurred while fetching teams: ", error);
+    throw error;
+  }
+}
